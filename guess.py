3f6bf432811b0e34da681b08251b52551fa5d1ff
Falon Darville <falondarville@gmail.com>
2018-07-10 14:09:54 -0700

    fix guess game

--- guess.py
+++ guess.py
@@ -5,16 +5,16 @@ random_number = random.randint(1, 10)
 # if the player guesses correctly, tell them they won
 # otherwise, tell them if they are too high or too low
 
-user_number = input("Please guess a number ")
+user_number = int(input("Please guess a number "))
 
-if random_number == user_number:
-	print("You won!")
-	user_input = input("Would you like to play again? Enter y or n")
-	if user_input == y:
-		user_number = input("Please guess a number ")
+while random_number != user_number:
+	if random_number < user_number:
+		print("You guessed too high!")
+		user_number = int(input("Please guess a number "))
 	else:
-		break
-elif random_number < user_number:
-	print("You guessed too high!")
-else:
-	print("You guessed too low!")
+		print("You guessed too low!")
+		user_number = int(input("Please guess a number "))
+print("You won!")
+user_input = input("Would you like to play again? Enter y or n: ")
+if user_input == "y":
+	user_number = int(input("Please guess a number "))
