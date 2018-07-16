ad20e3f53d8e5c8c0df498ec393c686456dc2e3e
Falon Darville <falondarville@gmail.com>
2018-07-15 17:01:30 -0700

    add functions with parameters file

---
+++ square.py
@@ -0,0 +1,12 @@
+# functions that accept parameters
+def square(num):
+	return num * num
+
+print(square(4))
+print(square(8))
+
+# function that takes multiple parameters
+def add(first, second):
+	return first + second
+
+print(add(3, 4))
