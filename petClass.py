d1fa28bfd2efd63e9ad28dff0eb7806dfdbb096f
Falon Darville <falondarville@gmail.com>
2018-08-05 23:02:01 -0700

    sample class with class variable list

---
+++ petClass.py
@@ -0,0 +1,18 @@
+class Pet:
+	allowed = ["cat", "dog", "fish", "rat"]
+	def __init__(self, name, species):
+		if species not in Pet.allowed:
+			raise ValueError(f"You can't have a {species} pet!")
+		self.name = name
+		self.species = species
+
+	def set_species(self, species):
+		if species not in Pet.allowed:
+			raise ValueError(f"You can't have a {species} pet!")
+		allowed = ["cat", "dog", "fish", "rat"]
+		self.species = species
+
+cat = Pet("Blue", "cat")
+dog = Pet("Wyatt", "dog")
+
+Pet("Fluffy", "Tiger")
