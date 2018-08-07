953eb376b37e6aae9c45e4cf6865b9a100ed8038
Falon Darville <falondarville@gmail.com>
2018-08-07 16:51:20 -0700

    create new file and add repr method

---
+++ repr.py
@@ -0,0 +1,46 @@
+class User:
+
+	active_users = 0
+
+	@classmethod
+	def display_active_users(cls):
+		return f"There are {cls.active_users} active users."
+
+	@classmethod
+	def from_string(cls, data_str):
+		first, last, age = data_str.split(',')
+		return cls(first, last, int(age))
+
+	def __init__(self, first, last, age):
+		self.first = first
+		self.last = last
+		self.age = age
+		User.active_users += 1
+
+	# we added a representation method. A repr method is defined by the developer. 
+	def __repr__(self):
+		return f"{self.first} is {self.age}"
+
+	def logout(self):
+		User.active_users -= 1
+		return f"{self.first} has logged out"
+
+	def full_name(self):
+		return f"{self.first} {self.last}"
+
+	def initials(self):
+		return f"{self.first[0]}.{self.last[0]}"
+
+	def likes(self, thing):
+		return f"{self.first} likes {thing}"
+
+	def is_senior(self):
+		return self.age >= 65
+
+	def birthday(self):
+		self.age += 1
+		return f"Happy {self.age}th birthday, {self.first}"
+
+user1 = User("Joe", "Smith", 123)
+user2 = User("Blanca", "Roberts", 657)
+
